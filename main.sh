7d88eb16aa603b581a1f8b98344505518c89220e
todtjs92 <todtjs92@naver.com>
2023-11-05 19:15:15 +0900

    add

--- main.sh
+++ main.sh
@@ -3,22 +3,22 @@
 set -euo pipefail
 
 # set virtual env 
-source ~/venv_rec/bin/activate
+venv_python=/home/todtjs92/venv_rec/bin/python
 project_path=/home/todtjs92/ggulmo_rec/ggulmo
 
 echo start 
 
 #preprocess
-python ${project_path}/preprocess/1.raw_log.py
-python ${project_path}/preprocess/1.raw_meta.py
-python ${project_path}/preprocess/2.add_view_to_meta.py
-python ${project_path}/preprocess/2.user_filter.py
-python ${project_path}/preprocess/3.category_add_to_meta.py
-python ${project_path}/preprocess/3.negative_impression.py
-python ${project_path}/preprocess/4.make_feature_table.py
+${venv_python} ${project_path}/preprocess/1.raw_log.py
+${venv_python} ${project_path}/preprocess/1.raw_meta.py
+${venv_python} ${project_path}/preprocess/2.add_view_to_meta.py
+${venv_python} ${project_path}/preprocess/2.user_filter.py
+${venv_python} ${project_path}/preprocess/3.category_add_to_meta.py
+${venv_python} ${project_path}/preprocess/3.negative_impression.py
+${venv_python} ${project_path}/preprocess/4.make_feature_table.py
 
 # model run and inference 
-python ${project_path}/models/FM/main.py
+${venv_python} ${project_path}/models/FM/main.py
 
 
 
