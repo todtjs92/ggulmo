ae623f51c42a88293eeebf354d303358164c30dd
todtjs92 <todtjs92@naver.com>
2023-11-05 18:49:00 +0900

    test

--- main.sh
+++ main.sh
@@ -4,19 +4,21 @@ set -euo pipefail
 
 # set virtual env 
 source ~/venv_rec/bin/activate
+project_path=/home/todtjs92/ggulmo_rec/ggulmo
+
 echo start 
 
 #preprocess
-python ./preprocess/1.raw_log.py
-python ./preprocess/1.raw_meta.py
-python ./preprocess/2.add_view_to_meta.py
-python ./preprocess/2.user_filter.py
-python ./preprocess/3.category_add_to_meta.py
-python ./preprocess/3.negative_impression.py
-python ./preprocess/4.make_feature_table.py
+python ${project_path}/preprocess/1.raw_log.py
+python ${project_path}/preprocess/1.raw_meta.py
+python ${project_path}/preprocess/2.add_view_to_meta.py
+python ${project_path}/preprocess/2.user_filter.py
+python ${project_path}/preprocess/3.category_add_to_meta.py
+python ${project_path}/preprocess/3.negative_impression.py
+python ${project_path}/preprocess/4.make_feature_table.py
 
 # model run and inference 
-python ./models/FM/main.py
+python ${project_path}/models/FM/main.py
 
 
 
